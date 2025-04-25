a28fe22935b349944fc2db8f22691a0e038a28d4
Luka Popovic <lukapopovic0111@gmail.com>
2025-04-25 23:11:04 +0200

    Function-choosingWiner

--- script.js
+++ script.js
@@ -18,100 +18,52 @@ const computerRock = document.querySelector('.rock-2');
 const computerText = document.querySelector('.computer');
 const result = document.querySelector('#result');
 const btnAgain = document.querySelector('#btn-again');
+const modal = document.querySelector('.modal');
+const btnOut = document.querySelector('#btn-out');
+const background = document.querySelector('.background');
 
 let seconds = 3;
 let computerPicked;
 let ruka = ['ROCK', 'SCISSORS', 'PAPER'];
-let random = ruka[Math.trunc(Math.random() * 3) + 0];
-let scores = 0;
-let paperValue = 2;
-let scissorsValue = 2;
-let rockValue = 2;
-let rockValuePlayer = 2;
-let paperValuePlayer = 2;
-let scissorsValuePlayer = 2;
-let computerValue;
-let playerValue;
+let resultScore = '';
+let scoreNumber = 0;
 let interval;
+let playerChoice = '';
+let computerChoice = ruka[Math.trunc(Math.random() * 3) + 0];
 
-btnPaper.addEventListener('click', function () {
-  hidden();
-  youPickedPaper.classList.remove('hidden');
-  youPickedText.textContent = 'PAPER';
-  interval = setInterval(function () {
-    timer();
-    if (seconds === 0) {
-      clearInterval(interval);
-      chosingWiner();
-      scoring();
-    }
-  }, 1000);
-  valueOfComputer();
-  valueOfPlayer('Paper');
-});
-btnScissors.addEventListener('click', function () {
-  hidden();
-  youPickedScissors.classList.remove('hidden');
-  youPickedText.textContent = 'SCISSORS';
-  interval = setInterval(function () {
-    timer();
-    if (seconds === 0) {
-      clearInterval(interval);
-      chosingWiner();
-      scoring();
-    }
-  }, 1000);
-  valueOfComputer();
-  valueOfPlayer('Scissors');
-});
-btnRock.addEventListener('click', function () {
-  hidden();
-  youPickedRock.classList.remove('hidden');
-  youPickedText.textContent = 'ROCK';
-  interval = setInterval(function () {
-    timer();
-    if (seconds === 0) {
-      clearInterval(interval);
-      chosingWiner();
-      scoring();
-    }
-  }, 1000);
-  valueOfComputer();
-  valueOfPlayer('Rock');
-});
-//
-function valueOfComputer() {
-  if (random === 'ROCK') {
-    rockValue = 2;
-    scissorsValuePlayer = scissorsValuePlayer - 1;
-    paperValuePlayer = paperValuePlayer + 1;
-    computerValue = rockValue;
-  } else if (random === 'PAPER') {
-    paperValue = 2;
-    scissorsValuePlayer = scissorsValue + 1;
-    rockValuePlayer--;
-    computerValue = paperValue;
-  } else if (random === 'SCISSORS') {
-    scissorsValue = 2;
-    paperValuePlayer--;
-    rockValuePlayer = rockValuePlayer + 1;
-    computerValue = scissorsValue;
-  }
-}
-function hidden() {
-  main.classList.add('hidden');
-  btnRules.classList.add('hidden');
-  miniBox.classList.remove('hidden');
-  computerText.classList.remove('hidden');
-}
 function chosingWiner() {
-  if (computerValue > playerValue) {
-    result.textContent = 'YOU LOSE';
-    backgroundColor(computerPaper, computerScissors, computerRock);
-  } else if (computerValue < playerValue) {
-    result.textContent = 'YOU WIN';
+  if (playerChoice === 'PAPER' && computerChoice === 'ROCK') {
+    resultScore = 'YOU WIN';
+    result.textContent = resultScore;
     backgroundColor(youPickedPaper, youPickedRock, youPickedScissors);
-  } else if (computerValue === playerValue) {
-    result.textContent = 'DRAW';
+  } else if (playerChoice === 'PAPER' && computerChoice === 'SCISSORS') {
+    resultScore = 'YOU LOSE';
+    result.textContent = resultScore;
+    backgroundColor(computerPaper, computerScissors, computerRock);
+  } else if (playerChoice === 'PAPER' && computerChoice === 'PAPER') {
+    resultScore = 'DRAW';
+    result.textContent = resultScore;
+  } else if (playerChoice === 'ROCK' && computerChoice === 'PAPER') {
+    resultScore = 'YOU LOSE';
+    result.textContent = resultScore;
+    backgroundColor(computerPaper, computerScissors, computerRock);
+  } else if (playerChoice === 'ROCK' && computerChoice === 'ROCK') {
+    resultScore = 'DRAW';
+    result.textContent = resultScore;
+  } else if (playerChoice === 'ROCK' && computerChoice === 'SCISSORS') {
+    resultScore = 'YOU WIN';
+    result.textContent = resultScore;
+    backgroundColor(youPickedPaper, youPickedRock, youPickedScissors);
+  } else if (playerChoice === 'SCISSORS' && computerChoice === 'SCISSORS') {
+    resultScore = 'DRAW';
+    result.textContent = resultScore;
+  } else if (playerChoice === 'SCISSORS' && computerChoice === 'PAPER') {
+    resultScore = 'YOU WIN';
+    result.textContent = resultScore;
+    backgroundColor(youPickedPaper, youPickedRock, youPickedScissors);
+  } else if (playerChoice === 'SCISSORS' && computerChoice === 'ROCK') {
+    resultScore = 'YOU LOSE';
+    result.textContent = resultScore;
+    backgroundColor(computerPaper, computerScissors, computerRock);
   }
 }
